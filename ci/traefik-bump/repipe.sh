34361040c016d6d5db8047ca2511a38ede34a0b7
Benjamin Gandon <benjamin@gstack.io>
2019-12-15 23:49:52 +0100

    Use secrets from Credhub instead of pushing them cleartext into pipeline definitions

--- ci/traefik-bump/repipe.sh
+++ ci/traefik-bump/repipe.sh
@@ -8,10 +8,10 @@ pushd "${SCRIPT_DIR}" > /dev/null
 
 (
     set -x
-    fly -t "gk" \
-        set-pipeline -p "traefik-bump" \
-        -c "traefik-bump-pipeline.yml" \
-        -l "../config.yml" -l "../secrets.yml"
+    fly --target="gk" \
+        set-pipeline --pipeline="traefik-bump" \
+        --config="traefik-bump-pipeline.yml" \
+        --load-vars-from="../config.yml"
 )
 
 popd > /dev/null

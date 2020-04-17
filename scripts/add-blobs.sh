c7ebe0812a30c1cbda62d1b010a5f69a8e36a466
Gstack Concourse Ninja <concourse-ninja@gstack.io>
2020-04-17 22:38:39 +0000

    Bump Traefik binary to version 1.7.24

--- scripts/add-blobs.sh
+++ scripts/add-blobs.sh
@@ -2,8 +2,8 @@
 
 set -eo pipefail -u -x
 
-TRAEFIK_VERSION=1.7.11
-TRAEFIK_SHA256=68332497361cbb694545833c7efb1f569db48a0e5265fad7ca75c91b900c3faa
+TRAEFIK_VERSION=1.7.24
+TRAEFIK_SHA256=01679e672722d3c419223132823a414bb11d3e44b5039d5c0b3f569fe7da2e3b
 
 if [[ ! -f "traefik-${TRAEFIK_VERSION}_linux-amd64" && ! -f "traefik-${TRAEFIK_VERSION}_linux-amd64.gz" ]]; then
     curl -L "https://github.com/containous/traefik/releases/download/v$TRAEFIK_VERSION/traefik_linux-amd64" \
